3bfc27a5c91d734aad23253bf8e3ff98d08e1325
bryan-prog <bryanoliva234@gmail.com>
2024-09-27 10:05:03 +0800

    first commit

---
+++ app/dashboard.tsx
@@ -0,0 +1,287 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, DrawerLayoutAndroid, Image, Dimensions } from 'react-native';
+import { MaterialCommunityIcons } from 'react-native-vector-icons';  
+import { useRouter } from 'expo-router';
+import { LinearGradient } from 'expo-linear-gradient';
+
+const { width: screenWidth } = Dimensions.get('window');  
+
+export default function Dashboard() {
+  const router = useRouter();
+  const [drawer, setDrawer] = useState(null);
+
+  function handleLogout() {
+    router.replace('/');
+  }
+
+  const navigationView = () => (
+    <View style={styles.sidebar}>
+      <View style={styles.logoContainer}>
+        <Image
+          source={require('../assets/images/DTS.png')} 
+          style={styles.logoImage}
+        />
+      </View>
+
+    
+      <View style={styles.menuItems}>
+        <TouchableOpacity style={styles.menuItem}>
+          <MaterialCommunityIcons name="view-dashboard" size={20} color="#000080" />
+          <Text style={styles.menuText}>Dashboard</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <MaterialCommunityIcons name="folder" size={20} color="#228B22" />
+          <Text style={styles.menuText}>Documents</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <MaterialCommunityIcons name="calendar-clock" size={20} color="#FF4500" />
+          <Text style={styles.menuText}>Pending for Release</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <MaterialCommunityIcons name="inbox-arrow-down" size={20} color="#B22222" />
+          <Text style={styles.menuText}>Incoming Documents</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <MaterialCommunityIcons name="check-box-outline" size={20} color="#FF8C00" />
+          <Text style={styles.menuText}>Tagged as Terminal</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <MaterialCommunityIcons name="account-multiple-plus" size={20} color="#1E90FF" />
+          <Text style={styles.menuText}>List / Register User</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.menuItem}>
+          <MaterialCommunityIcons name="qrcode" size={20} color="#FF6347" />
+          <Text style={styles.menuText}>QR Manager</Text>
+        </TouchableOpacity>
+      </View>
+
+      <TouchableOpacity style={styles.logoutButton} onPress={handleLogout}>
+        <MaterialCommunityIcons name="logout" size={20} color="white" />
+        <Text style={styles.logoutButtonText}>Logout</Text>
+      </TouchableOpacity>
+    </View>
+  );
+
+  return (
+    <DrawerLayoutAndroid
+      ref={(drawer) => setDrawer(drawer)}
+      drawerWidth={250}
+      drawerPosition="left"
+      renderNavigationView={navigationView}
+    >
+      <View style={styles.container}>
+        <View style={styles.headerContainer}>
+          <TouchableOpacity onPress={() => drawer.openDrawer()}>
+            <MaterialCommunityIcons name="menu" size={36} color="white" />
+          </TouchableOpacity>
+
+          <View style={styles.navbarImages}>
+            <Image
+              source={require("../assets/images/bagong-pilipinas.png")}
+              style={styles.navbarImage}
+            />
+            <Image
+              source={require("../assets/images/sjc.png")}
+              style={styles.navbarImage}
+            />
+          </View>
+        </View>
+
+        <ScrollView contentContainerStyle={styles.scrollViewContent}>
+          <View style={styles.card}>
+            <View>
+              <Text style={styles.cardTitle}>RELEASED</Text>
+              <Text style={styles.cardCount}>14</Text>
+              <Text style={styles.cardSubtitle}>Last Updated</Text>
+            </View>
+            <LinearGradient
+              colors={["#FF7E5F", "#FD3A4A"]}
+              style={styles.iconBackground}
+            >
+              <MaterialCommunityIcons name="folder" size={40} color="white" />
+            </LinearGradient>
+          </View>
+
+          <View style={styles.card}>
+            <View>
+              <Text style={styles.cardTitle}>FOR RELEASE</Text>
+              <Text style={styles.cardCount}>1</Text>
+              <Text style={styles.cardSubtitle}>Last Updated</Text>
+            </View>
+            <LinearGradient
+              colors={["#FFB74D", "#FFA726"]}
+              style={styles.iconBackground}
+            >
+              <MaterialCommunityIcons
+                name="cloud-upload"
+                size={40}
+                color="white"
+              />
+            </LinearGradient>
+          </View>
+
+          <View style={styles.card}>
+            <View>
+              <Text style={styles.cardTitle}>INCOMING</Text>
+              <Text style={styles.cardCount}>7</Text>
+              <Text style={styles.cardSubtitle}>Last Updated</Text>
+            </View>
+            <LinearGradient
+              colors={["#66BB6A", "#43A047"]}
+              style={styles.iconBackground}
+            >
+              <MaterialCommunityIcons
+                name="inbox-arrow-down"
+                size={40}
+                color="white"
+              />
+            </LinearGradient>
+          </View>
+
+          <View style={styles.card}>
+            <View>
+              <Text style={styles.cardTitle}>RECEIVED</Text>
+              <Text style={styles.cardCount}>15</Text>
+              <Text style={styles.cardSubtitle}>Last Updated</Text>
+            </View>
+            <LinearGradient
+              colors={["#42A5F5", "#1E88E5"]}
+              style={styles.iconBackground}
+            >
+              <MaterialCommunityIcons name="inbox" size={40} color="white" />
+            </LinearGradient>
+          </View>
+          <View style={styles.card}>
+            <View>
+              <Text style={styles.cardTitle}>TERMINAL</Text>
+              <Text style={styles.cardCount}>6</Text>
+              <Text style={styles.cardSubtitle}>Last Updated</Text>
+            </View>
+        
+          <LinearGradient
+            colors={["#AA4465", "#923CB5"]}
+            style={styles.iconBackground}
+          >
+            <MaterialCommunityIcons
+              name="check-bold"
+              size={40}
+              color="white"
+            />
+          </LinearGradient>
+          </View>
+        </ScrollView>
+      </View>
+    </DrawerLayoutAndroid>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+  headerContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: '#000',
+    width: '100%',
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+  },
+  navbarImages: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  navbarImage: {
+    width: 50,
+    height: 50,
+    marginLeft: 10,
+  },
+  scrollViewContent: {
+    width: '100%',
+    padding: 10,
+    backgroundColor: '#F5F5F5',
+    flexGrow: 1,
+  },
+  sidebar: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: '#2E3B55',
+    justifyContent: 'space-between',
+  },
+  logoContainer: {
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  logoImage: {
+    width: 150, 
+    height: 50,
+    resizeMode: 'contain',
+  },
+  menuItems: {
+    flex: 1,
+  },
+  menuItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+  },
+  menuText: {
+    color: '#fff',
+    fontSize: 16,
+    marginLeft: 10,
+  },
+  logoutButton: {
+    backgroundColor: '#D9534F',
+    padding: 10,
+    alignItems: 'center',
+    borderRadius: 5,
+    flexDirection: 'row',
+  },
+  logoutButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    marginLeft: 10,
+  },
+  card: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%',
+    padding: 20,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 4 },
+    shadowOpacity: 0.3,
+    shadowRadius: 10,
+    marginBottom: 20,
+    elevation: 8,
+    borderLeftWidth: 3,
+    borderLeftColor: '#D3D3D3',
+  },
+  cardTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 5,
+  },
+  cardCount: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#333',
+    marginBottom: 5,
+  },
+  cardSubtitle: {
+    fontSize: 14,
+    color: '#666',
+  },
+  iconBackground: {
+    width: 80, 
+    height: 80,
+    borderRadius: 40, 
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
---
+++ app/index.tsx
@@ -0,0 +1,351 @@
+import React, { useState, useEffect } from 'react';
+import { SafeAreaView, StyleSheet, Text, View, TextInput, TouchableOpacity, ImageBackground, Image, Modal, KeyboardAvoidingView, ScrollView, Platform, ActivityIndicator } from 'react-native';
+import { useRouter } from 'expo-router';
+import { LinearGradient } from 'expo-linear-gradient';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Dimensions } from 'react-native';
+import NetInfo from '@react-native-community/netinfo';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+const { width, height } = Dimensions.get('window');
+
+export default function LoginScreen() {
+  const [username, setUserName] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const [modalVisible, setModalVisible] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const [isConnected, setIsConnected] = useState(true);
+  const [showNoInternetModal, setShowNoInternetModal] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const router = useRouter();
+
+  useEffect(() => {
+    const unsubscribe = NetInfo.addEventListener(state => {
+      setIsConnected(state.isConnected);
+      if (!state.isConnected) {
+        setShowNoInternetModal(true);
+      } else {
+        setShowNoInternetModal(false);
+      }
+    });
+
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  useEffect(() => {
+    if (error) {
+      const timeout = setTimeout(() => {
+        setError('');
+      }, 3000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [error]);
+
+  async function handleLogin() {
+    if (!username || !password) {
+      setError('Please enter both username and password.');
+      return;
+    }
+
+    if (!isConnected) {
+      setError('No internet connection.');
+      return;
+    }
+
+    try {
+      setLoading(true);
+      const response = await fetch('https://reqres.in/api/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: username,
+          password: password,
+        }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        router.navigate('/dashboard');
+      } else {
+        setError(data.error || 'Invalid login credentials.');
+      }
+    } catch (error) {
+      setError('Failed to connect. Please try again.');
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <KeyboardAvoidingView
+        style={{ flex: 1 }}
+        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      >
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }} keyboardShouldPersistTaps="handled">
+          <ImageBackground source={require('../assets/images/bg.png')} style={styles.background}>
+            <View style={styles.logoContainer}>
+              <Image source={require('../assets/images/bagong-pilipinas.png')} style={styles.logo} />
+              <Image source={require('../assets/images/sjc.png')} style={styles.logo} />
+              <Image source={require('../assets/images/mayor.png')} style={styles.logo} />
+              <Image source={require('../assets/images/icto.png')} style={styles.logo} />
+            </View>
+
+            <Text style={styles.welcomeText}>Welcome!</Text>
+
+            <LinearGradient
+              colors={['#001f3f', '#001f3f']}
+              start={{ x: 0, y: 0 }}
+              end={{ x: 1, y: 1 }}
+              style={styles.loginBox}
+            >
+              <Text style={styles.title}>DOCUMENT TRACKING SYSTEM</Text>
+
+              <View style={styles.inputContainer}>
+                <Icon name="user" size={20} color="#fff" style={styles.icon} />
+                <TextInput
+                  style={styles.input}
+                  placeholder="username"
+                  placeholderTextColor="rgba(255, 255, 255, 0.7)"
+                  autoCapitalize="none"
+                  value={username}
+                  onChangeText={setUserName}
+                />
+              </View>
+
+              <View style={styles.inputContainer}>
+                <Icon name="lock" size={20} color="#fff" style={styles.icon} />
+                <TextInput
+                  style={styles.input}
+                  placeholder="password"
+                  placeholderTextColor="rgba(255, 255, 255, 0.7)"
+                  secureTextEntry={!showPassword}
+                  value={password}
+                  onChangeText={setPassword}
+                />
+
+                <TouchableOpacity onPress={() => setShowPassword(!showPassword)}>
+                  <Icon name={showPassword ? 'eye' : 'eye-slash'} size={20} color="#fff" />
+                </TouchableOpacity>
+              </View>
+
+              <TouchableOpacity style={styles.loginButton} onPress={handleLogin} disabled={loading}>
+                {loading ? (
+                  <ActivityIndicator size="small" color="#fff" />
+                ) : (
+                  <Text style={styles.loginButtonText}>Login</Text>
+                )}
+              </TouchableOpacity>
+
+              {error ? <Text style={styles.errorText}>{error}</Text> : null}
+            </LinearGradient>
+
+            <TouchableOpacity onPress={() => setModalVisible(true)} style={styles.imageButton}>
+              <Image source={require('../assets/images/seal2.png')} style={styles.npcImage} />
+            </TouchableOpacity>
+
+            <Modal
+              animationType="none"
+              transparent={true}
+              visible={modalVisible}
+              onRequestClose={() => {
+                setModalVisible(!modalVisible);
+              }}
+            >
+              <TouchableOpacity
+                style={styles.modalOverlay}
+                activeOpacity={1}
+                onPress={() => setModalVisible(false)}
+              >
+                <TouchableOpacity style={styles.modalContent} activeOpacity={1} onPress={() => { }}>
+                  <TouchableOpacity onPress={() => setModalVisible(false)} style={styles.closeButton}>
+                    <Text style={styles.closeButtonText}>X</Text>
+                  </TouchableOpacity>
+                  <Image source={require('../assets/images/npc.jpg')} style={styles.npcModalImage} />
+                </TouchableOpacity>
+              </TouchableOpacity>
+            </Modal>
+
+           
+            <Modal
+              transparent={true}
+              visible={showNoInternetModal}
+              animationType="fade"
+              onRequestClose={() => setShowNoInternetModal(false)}
+            >
+              <View style={styles.modalOverlay}>
+                <View style={styles.noInternetModal}>
+                  <MaterialCommunityIcons
+                    name="wifi-off"
+                    size={50}
+                    color="#ff0000"
+                    style={styles.noWifiIcon}
+                  />
+                  <Text style={styles.noInternetText}>No Internet Connection</Text>
+                  <TouchableOpacity
+                    style={styles.okButton}
+                    onPress={() => setShowNoInternetModal(false)}
+                  >
+                    <Text style={styles.okButtonText}>OK</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            </Modal>
+          </ImageBackground>
+        </ScrollView>
+      </KeyboardAvoidingView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  logoContainer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginBottom: 0,
+    marginTop: 80,
+  },
+  logo: {
+    width: 75,
+    height: 80,
+    margin: 3,
+  },
+  welcomeText: {
+    fontSize: 24,
+    color: '#fff',
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+  loginBox: {
+    width: width * 0.9,
+    borderRadius: 20,
+    padding: 30,
+    alignItems: 'center',
+    marginTop: 30,
+  },
+  title: {
+    fontSize: 16,
+    color: '#fff',
+    marginBottom: 40,
+    fontWeight: 'bold',
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '100%',
+    height: 50,
+    backgroundColor: 'rgba(255, 255, 255, 0.1)',
+    borderRadius: 10,
+    marginBottom: 20,
+    paddingHorizontal: 15,
+    borderColor: 'rgba(255, 255, 255, 0.2)',
+    borderWidth: 1,
+  },
+  icon: {
+    marginRight: 10,
+  },
+  input: {
+    flex: 1,
+    color: '#fff',
+    fontSize: 16,
+  },
+  loginButton: {
+    width: width * 0.75,
+    height: 50,
+    backgroundColor: '#4B0082',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  loginButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  errorText: {
+    color: 'red',
+    marginTop: 10,
+    fontSize: 13,
+  },
+  imageButton: {
+    marginTop: 20,
+    alignItems: 'center',
+  },
+  npcImage: {
+    width: 120,
+    height: 110,
+    resizeMode: 'contain',
+    backgroundColor: 'transparent',
+  },
+  modalOverlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+  },
+  modalContent: {
+    width: width * 0.9,
+    height: height * 0.35,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 20,
+    alignItems: 'center',
+  },
+  closeButton: {
+    position: 'absolute',
+    top: 10,
+    right: 10,
+  },
+  closeButtonText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#000',
+  },
+  npcModalImage: {
+    width: '100%',
+    height: '100%',
+    resizeMode: 'contain',
+  },
+  noInternetModal: {
+    width: width * 0.8,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 30,
+  },
+  noInternetText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    color: '#000',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  noWifiIcon: {
+    marginBottom: 20, 
+  },
+  okButton: {
+    backgroundColor: '#4B0082',
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    borderRadius: 5,
+  },
+  okButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
